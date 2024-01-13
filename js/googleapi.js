f5c2148c3e297fa858ddfa32ebbf313a83a1950f
yansuogaming <yansuogaming@gmail.com>
2024-01-13 09:30:50 +0700

    Update

--- js/googleapi.js
+++ js/googleapi.js
@@ -1,45 +1,51 @@
-$(document).ready(function() {
-  $('.sub-menu').hide(); // Ẩn danh sách con ban đầu
-  $('.nav-link.link-body-emphasis').click(function(e) {
-      e.preventDefault();
-      $(this).next('.sub-menu').slideToggle(300); // Hiển thị/ẩn danh sách con khi nhấp vào danh mục
+// Thay thế YOUR_CLIENT_ID bằng Client ID bạn đã nhận được từ bước 2
+var CLIENT_ID = '148633961854-9g61vjq4hl25vop77uei23nmnmrrd4ak.apps.googleusercontent.com';
+
+// Các phạm vi (scopes) cần truy cập từ Google Drive API
+var SCOPES = ['https://www.googleapis.com/auth/drive.readonly'];
+
+// Hàm xử lý khi người dùng nhấp vào nút "Đăng nhập"
+function handleAuthClick() {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+// Hàm xử lý sau khi xác thực thành công
+function handleAuthResult(authResult) {
+  if (authResult && !authResult.error) {
+    // Xác thực thành công, gọi hàm loadDriveAPI để tải Google Drive API
+    loadDriveAPI();
+  }
+}
+
+// Hàm tải Google Drive API
+function loadDriveAPI() {
+  gapi.client.load('drive', 'v3', listFiles);
+}
+
+// Hàm lấy danh sách file và folder từ Google Drive API
+function listFiles() {
+  gapi.client.drive.files.list({
+    'pageSize': 10, // Số lượng file/folder muốn lấy
+    'fields': 'nextPageToken, files(id, name, mimeType)' // Các trường thông tin muốn lấy
+  }).then(function(response) {
+    var files = response.result.files;
+    // Xử lý danh sách files tại đây
+    console.log(files);
   });
+}
 
-  function loadDriveApi() {
-      gapi.load('client', initClient);
-  }
+// Khởi tạo gapi và xác thực
+function initClient() {
+  gapi.client.init({
+    'clientId': CLIENT_ID,
+    'scope': SCOPES.join(' '),
+    'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest']
+  }).then(function() {
+    // Xác thực thành công, gọi hàm handleAuthResult
+    gapi.auth2.getAuthInstance().isSignedIn().listen(handleAuthResult);
+    handleAuthResult(gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse());
+  });
+}
 
-  function initClient() {
-      gapi.client.init({
-          apiKey: 'AIzaSyCwt9Dcmc_Vs0aPmWssTkhRdM-Df_Gsg3A', // Thay YOUR_API_KEY bằng API key của bạn
-          discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
-      }).then(function() {
-          listPublicFolders();
-      }).catch(function(error) {
-          console.log('Error initializing the client: ' + error);
-      });
-  }
-
-  function listPublicFolders() {
-      gapi.client.drive.files.list({
-          q: "mimeType='application/vnd.google-apps.folder' and visibility='public' and trashed=false",
-          fields: 'files(id, name)',
-      }).then(function(response) {
-          var folders = response.result.files;
-          var folderList = document.getElementById('folder-list');
-
-          for (var i = 0; i < folders.length; i++) {
-              var folder = folders[i];
-              var folderLink = document.createElement('a');
-              folderLink.href = 'https://drive.google.com/drive/folders/' + folder.id;
-              folderLink.textContent = folder.name;
-              folderList.appendChild(folderLink);
-              folderList.appendChild(document.createElement('br'));
-          }
-      }).catch(function(error) {
-          console.log('Error listing public folders: ' + error);
-      });
-  }
-
-  loadDriveApi();
-});
+// Gọi hàm initClient khi thư viện gapi đã được tải
+gapi.load('client:auth2', initClient);
